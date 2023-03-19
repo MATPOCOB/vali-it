ed8cd6a0d9048bf248ad97d3f20991264977df39
Kirill Klenski <kirill.klenski@gmail.com>
2023-03-19 20:02:45 +0200

    add sample of http server

---
+++ src/HttpServerTutorial.java
@@ -0,0 +1,32 @@
+import com.sun.net.httpserver.*;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.time.Instant;
+
+public class HttpServerTutorial {
+  public static void main(String[] args) throws IOException {
+    InetSocketAddress addr = new InetSocketAddress("localhost", 5555);
+    HttpServer httpServer = HttpServer.create(addr, 0);
+    httpServer.createContext("/status", new RequestHandler());
+    httpServer.start();
+    System.out.println("Server started, listening at: " + addr);
+  }
+
+  private static class RequestHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+      if ("GET".equals(exchange.getRequestMethod())) {
+        System.out.println(exchange.getRequestURI());
+        String response = Instant.now().toString();
+
+        exchange.sendResponseHeaders(200, response.length());
+
+        try (OutputStream responseBody = exchange.getResponseBody()) {
+          responseBody.write(response.getBytes());
+        }
+      }
+    }
+  }
+}
